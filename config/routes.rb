aa5a7d482e56db69527d7b67b7f6f8d7dd9ae30e
Romnick Alegonza <kinmor06@jarvis-Mrk06.(none)>
2012-12-31 02:40:12 +0800

    reverted test printing, used local file

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ GoogleCloudPrintExample::Application.routes.draw do
     end
   end
 
+  match 'test-print/:id' => 'printers#queue_test', :as => :test_print
   match '/auth/google_oauth2/callback' => 'sessions#create'
   match '/logout' => 'sessions#destroy'
   
